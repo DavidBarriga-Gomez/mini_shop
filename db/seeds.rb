3d5b54fed945c2bfd964a77b80464c95c1d9f5db
David Gomez <davdg25@gmail.com>
2019-10-23 08:41:15 -0600

    created Merchant Index page/testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#merchants
+merchant_1 = Merchant.new(name: 'David', address: '4942 willow street', city: 'Denver', state: 'Colorado', zip: '80238')
+merchant_2 = Merchant.new(name: 'Scott', address: '4942 willow street', city: 'Denver', state: 'Colorado', zip: '80238')
+merchant_3 = Merchant.new(name: 'Ollie', address: '4942 willow street', city: 'Denver', state: 'Colorado', zip: '80238')
+merchant_4 = Merchant.new(name: 'Ryan', address: '4942 willow street', city: 'Denver', state: 'Colorado', zip: '80238')
